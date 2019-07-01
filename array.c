3ee47a762ad601bc49f64a352fda2adedc66602d
Antonio Angel Martínez Domínguez <aamd79@gmail.com>
2019-07-01 17:58:51 +0100

    Update array.c
    It manages the memory resources needed to create and add resources in the objects and array.
    The types of data handled by the API are described below.
    
    NULL:
    Null object, for example, to indicate the end of the collection.
    The MiniBox API saves this value as null = 0.
    
    BOOLEAN:
    Typical true and false values ​​of c.
    The MiniBox API saves this value as true = 1, false = 0.
    
    NUMBER:
    All numeric values ​​will be of type double.
    In the representations, the MiniBox API will determine whether to add the decimal part or only the entire part.
    Examples: (1.0 = 1) (3.900 = 3.9)
    
    STRING:
    UTF-8 character string ending in zero of c.
    For the dynamic memory that you want to discard, we will add the
    MINIBOX_MEMORY_RELEASE parameter.
    If we do not want the MiniBox API to free the memory or this is a constant, we will add the MINIBOX_MEMORY_RETAINT parameter.
    Example: "This is an example of a string".
    
    ARRAY and OBJECT:
    Like STRING, it will be necessary to indicate to the MiniBox API how the memory will be managed.
    MINIBOX_MEMORY_RELEASE will remove the object and check its contents if it should also be deleted.
    MINIBOX_MEMORY_RETAINT keeps the object in memory.

--- array.c
+++ array.c
@@ -1,27 +1,163 @@
-It manages the memory resources needed to create and add resources in the objects and array.
-The types of data handled by the API are described below.
+ //
+//  array.c
+//  minibox
+//
+//  Created by Antonio Angel Martínez Domínguez on 1/6/19.
+//
+//  Copyright 2019 Rokit Systems
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
-NULL:
-Null object, for example, to indicate the end of the collection.
-The MiniBox API saves this value as null = 0.
+#include "box.h"
 
-BOOLEAN:
-Typical true and false values ​​of c.
-The MiniBox API saves this value as true = 1, false = 0.
+#define arrset(p) \
+    box_set(__box, p + MINIBOX_VALUE, __value); \
+    box_set(__box, p + MINIBOX_TYPE , &__type)
 
-NUMBER:
-All numeric values ​​will be of type double.
-In the representations, the MiniBox API will determine whether to add the decimal part or only the entire part.
-Examples: (1.0 = 1) (3.900 = 3.9)
+box_t new_array()
+{
+    return box_create(MINIBOX_TYPE_ARRAY);
+}
 
-STRING:
-UTF-8 character string ending in zero of c.
-For the dynamic memory that you want to discard, we will add the
-MINIBOX_MEMORY_RELEASE parameter.
-If we do not want the MiniBox API to free the memory or this is a constant, we will add the MINIBOX_MEMORY_RETAINT parameter.
-Example: "This is an example of a string".
+void* array_get(box_t __box, long __index)
+{
+    return BOXB + (__index * V2S);
+}
 
-ARRAY and OBJECT:
-Like STRING, it will be necessary to indicate to the MiniBox API how the memory will be managed.
-MINIBOX_MEMORY_RELEASE will remove the object and check its contents if it should also be deleted.
-MINIBOX_MEMORY_RETAINT keeps the object in memory.
+void array_remove(box_t __box, long __index)
+{
+    long p = __index * V2S;
+    
+    box_free_value(BOXB + p);
+    
+    if (box_move(__box, p + V2S, p, (BOXS - V2S) - p))
+        return;
+    
+    if (box_reallocated(__box, -V2S)) return;
+}
+
+long array_count(box_t __box)
+{
+    return BOXS / V2S;
+}
+
+#pragma mark - Set
+
+void array_set(box_t __box, long __position, const void *__value, long __type)
+{
+    box_free_value(box_get(__box, __position));
+    
+    arrset(__position);
+}
+
+void array_set_boolean(box_t __box, long __index, long __value)
+{
+    array_set(__box, __index * V2S, &__value, MINIBOX_TYPE_BOOLEAN);
+}
+
+void array_set_null(box_t __box, long __index)
+{
+    long v =0;
+    array_set(__box, __index * V2S, &v, MINIBOX_TYPE_NULL);
+}
+
+void array_set_number(box_t __box, long __index, long __value)
+{
+    array_set(__box, __index * V2S, &__value, MINIBOX_TYPE_NUMBER);
+}
+
+void array_set_string(box_t __box, long __index, int __vmt, const char *__value)
+{
+    array_set(__box, __index * V2S, &__value, MINIBOX_TYPE_STRING + __vmt);
+}
+
+void array_set_box(box_t __box, long __index, int __vmt, long __value)
+{
+    array_set(__box, __index * V2S, &__value, box_type(__value) + __vmt);
+}
+
+#pragma mark - Add
+
+void array_add(box_t __box, const void *__value, long __type)
+{
+    if (box_reallocated(__box, V2S)) return;
+    
+    arrset(BOXS - V2S);
+}
+
+void array_add_boolean(box_t __box, long __value)
+{
+    array_add(__box, &__value, MINIBOX_TYPE_BOOLEAN);
+}
+
+void array_add_null(box_t __box)
+{
+    long value = 0;
+    array_add(__box, &value, MINIBOX_TYPE_NULL);
+}
+
+void array_add_number(box_t __box, double __value)
+{
+    array_add(__box, &__value, MINIBOX_TYPE_NUMBER);
+}
+
+void array_add_string(box_t __box, int __vmt, const char *__value)
+{
+    array_add(__box, &__value, MINIBOX_TYPE_STRING + __vmt);
+}
+
+void array_add_box(box_t __box, int __vmt, box_t __value)
+{
+    array_add(__box, &__value, box_type(__value) + __vmt);
+}
+
+#pragma mark - Insert
+
+void array_insert(box_t __box, long __index, const void *__value, long __type)
+{
+    long p = __index * V2S;
+    
+    if (box_reallocated(__box, V2S)) return;
+    
+    if (box_move(__box, p, p + V2S, (BOXS - V2S) - p))
+        return;
+    
+    arrset(p);
+}
+
+void array_insert_boolean(box_t __box, long __index, long __value)
+{
+    array_insert(__box, __index, &__value, MINIBOX_TYPE_BOOLEAN);
+}
+
+void array_insert_null(box_t __box, long __index)
+{
+    long value = 0;
+    array_insert(__box, __index, &value, MINIBOX_TYPE_NULL);
+}
+
+void array_insert_number(box_t __box, long __index, double __value)
+{
+    array_insert(__box, __index, &__value, MINIBOX_TYPE_NUMBER);
+}
+
+void array_insert_string(box_t __box, long __index, int __vmt, const char *__value)
+{
+    array_insert(__box, __index, &__value, MINIBOX_TYPE_STRING + __vmt);
+}
+
+void array_insert_box(box_t __box, long __index, int __vmt, box_t __value)
+{
+    array_insert(__box, __index, &__value, box_type(__value) + __vmt);
+}
